4947ab77d939ef3f1bf4cdc4bbab03afb490d54e
Art <deckuser@hotmail.com>
2020-01-19 14:56:47 +0400

    _

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yield
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            
+            foreach (string element in UserCollection.Power())
+            {
+                Console.WriteLine(element);
+            }
+
+            IEnumerable enumerable = UserCollection.Power();
+
+            IEnumerator enumerator = enumerable.GetEnumerator();
+
+            while (enumerator.MoveNext())
+            {
+                String element = enumerator.Current as String;
+
+                Console.WriteLine(element);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
